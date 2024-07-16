75a1b31bf6034e9f04d3a9ada3c38bd3250a658e
Artur <parkour@Arturs-Mac-mini.local>
2024-07-16 16:50:27 +0300

    Allow to update external configurations

--- File Util.js
+++ File Util.js
@@ -10,14 +10,15 @@ const root = {
         "Resources"
     ]),
     
-    
     updateConfiguration: async (fileName, content) => {
+        await root.updateExtConfiguration(fileName, content, Script.name())
+    },
+    
+    updateExtConfiguration: async (fileName, content, scriptName) => {
         
         const manager = root.manager
         const targetDirectory = root.joinPaths(
-            [root.getScriptableDir(), Script.name()])
-        
-        content = Data.fromString(String(content))
+            [root.getScriptableDir(), scriptName])
         
         if (!manager.isDirectory(targetDirectory)) {
             manager.createDirectory(targetDirectory, true)
@@ -25,7 +26,7 @@ const root = {
         
         const targetFile = manager.joinPath(targetDirectory, fileName)
             
-        await manager.write(targetFile, content)
+        await manager.write(targetFile, root.castToData(content))
     },
     
     getConfiguration: (fileName, defaultValue) => {
@@ -36,12 +37,27 @@ const root = {
         return root.doGetConfiguration(fileName, defaultValue, scriptName)
     },
     
+    findExtConfigurations: (fileNameRegex, scriptName) => {
+        
+        let scriptDir = root.joinPaths(
+            [root.getScriptableDir(), scriptName]
+        )
+        
+        if (!root.manager.isDirectory(scriptDir)) {
+            return []
+        }
+        
+        return root.manager.listContents(scriptDir)
+            .filter(fileName => fileName.match(fileNameRegex))
+    },
+    
     doGetConfiguration: (fileName, defaultValue, scriptName) => {
         const targetFile = root.joinPaths(
             [root.getScriptableDir(), scriptName, fileName]
         )
         
         if (!root.manager.fileExists(targetFile)) {
+            
             console.warn("File does not exist: " + targetFile)
             return defaultValue
         }
@@ -49,6 +65,10 @@ const root = {
         return root.manager.readString(targetFile)
     },
     
+    
+    castToData: content => {
+        return Data.fromString(String(content))
+    },
     
     joinPaths: paths => {
         let resultPath = ""
@@ -62,5 +82,7 @@ const root = {
 }
 
 module.exports.updateConfiguration = root.updateConfiguration
+module.exports.updateExtConfiguration = root.updateExtConfiguration
 module.exports.getConfiguration = root.getConfiguration
 module.exports.getExtConfiguration = root.getExtConfiguration
+module.exports.findExtConfigurations = root.findExtConfigurations
