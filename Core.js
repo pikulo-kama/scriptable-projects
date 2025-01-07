e498acd992fd2a00f6ae3bf2f2d9f34a12dff163
Artur Drabazha <jhorr2222@gmail.com>
2025-01-07 03:41:50 +0200

    Create global protected data holder

--- Core.js
+++ Core.js
@@ -4,6 +4,87 @@
 
 
 /**
+ * Used to hold protected fields of instances
+ * that should be accessible by other classes in script..
+ *
+ * @class ProtectedDataHolder
+ */
+class ProtectedDataHolder {
+
+    static #stateHolder = new WeakMap();
+
+    /**
+     * Used to add whole object with
+     * protected fields to state.
+     *
+     * @static
+     * @param {*} stateOwner instance that is owner of provided state
+     * @param {*} state object with protected data
+     * @memberof ProtectedDataHolder
+     */
+    static addToStateMultiple(stateOwner, state) {
+        const currentState = this.getState(stateOwner);
+        this.#stateHolder.set(stateOwner, {...currentState, ...state});
+    }
+    
+    /**
+     * Used to add single protected field
+     * to state.
+     *
+     * @static
+     * @param {*} stateOwner instance that is owner of provided state
+     * @param {*} name name of protected field
+     * @param {*} name value of protected field
+     * @memberof ProtectedDataHolder
+     */
+    static addToState(stateOwner, name, value) {
+        const currentState = this.getState(stateOwner);
+        this.#stateHolder.set(stateOwner, {...currentState, [name]: value});
+    }
+
+    /**
+     * Used to get protected field value
+     * from state.
+     *
+     * @static
+     * @param {*} stateOwner instance that is owner of provided state
+     * @param {*} name name of protected field
+     * @param {*} defaultValue value returned when field is not in state
+     * @return {*} value of protected field
+     * @memberof ProtectedDataHolder
+     */
+    static getFromState(stateOwner, name, defaultValue) {
+        let stateValue = this.getState(stateOwner)[name];
+
+        if (!stateValue) {
+            stateValue = defaultValue;
+        }
+
+        return stateValue;
+    }
+
+    /**
+     * Used to get state for the owner.
+     * If doesn't exist will create empty state.
+     *
+     * @param {*} stateOwner instance that is owner state
+     * @return {*} state
+     * @memberof ProtectedDataHolder
+     */
+    static getState(stateOwner) {
+        let state = this.#stateHolder.get(stateOwner);
+
+        if (!state) {
+            state = {};
+            this.#stateHolder.set(stateOwner, state);
+        }
+
+        return state;
+    }
+}
+
+
+/**
  * Wrapper function that injects
  * properties and state of classes into another one.
  * 
@@ -29,6 +110,29 @@ function Classes(...bases) {
 }
 
 
+function addToState(stateOwner, state) {
+    ProtectedDataHolder.addToStateMultiple(stateOwner, state);
+}
+
+
+function addOneToState(stateOwner, name, value) {
+    ProtectedDataHolder.addToState(stateOwner, name, value);
+}
+
+
+function getFromState(stateOwner, name, defaultValue) {
+    return ProtectedDataHolder.getFromState(stateOwner, name, defaultValue);
+}
+
+function getState(stateOwner) {
+    return ProtectedDataHolder.getState(stateOwner);
+}
+
+
 module.exports = {
-    Classes
+    Classes,
+    addToState,
+    addOneToState,
+    getFromState,
+    getState
 };
