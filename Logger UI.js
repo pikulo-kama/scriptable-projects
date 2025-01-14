1dd69cbe62f86b8386007533411838f0f1348113
Artur Drabazha <jhorr2222@gmail.com>
2025-01-14 23:43:31 +0200

    Add logging scripts

---
+++ Logger UI.js
@@ -0,0 +1,115 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: purple; icon-glyph: file-alt;
+
+const { FileUtil } = importModule("File Util");
+const { LogLevel } = importModule("Logger");
+const {
+    TextDataField,
+    UIDataTable,
+    UIForm,
+    UIFormField,
+    UIFormAction,
+    UIDeleteRowField
+} = importModule("CRUD Module");
+
+
+LOGGER_SCRIPT_NAME = "Logger";
+LEVELS_FILE_NAME = "levels.json";
+
+/**
+ * ENTRY POINT
+ */
+async function main() {
+
+    const tableBuilder = new LoggingTable();
+    const table = tableBuilder.build();
+
+    await table.present();
+}
+
+
+/**
+ * Used to render table used to edit log levels
+ * for services.
+ *
+ * @class LoggingTable
+ */
+class LoggingTable {
+
+    #serviceNameDataField = new TextDataField("service", "Service");
+    #logLevelDataField = new TextDataField("level", LogLevel.INFO);
+
+    /**
+     * Used to build UI table.
+     *
+     * @return {UITable} table
+     * @memberof LoggingTable
+     */
+    build() {
+
+        const table = new UIDataTable();
+        table.title = "Logging Levels";
+
+        table.allowCreation();
+        table.showSeparators();
+
+        table.setTableData(FileUtil.readJson(LOGGER_SCRIPT_NAME, LEVELS_FILE_NAME, {levels: []}).levels);
+        table.onDataModification((levels) => FileUtil.updateJson(LOGGER_SCRIPT_NAME, LEVELS_FILE_NAME, {levels}));
+
+        table.setDataFields([this.#serviceNameDataField, this.#logLevelDataField]);
+        table.setUIFields(this.#getUIFields());
+
+        return table;
+    }
+
+    /**
+     * Used to get list of UI fields that needs to be
+     * displayed in table.
+     *
+     * @return {List<UIField>} list of UI fields
+     * @memberof LoggingTable
+     */
+    #getUIFields() {
+
+        // Service Name Field
+        const serviceNameUIField = new UIForm((log) => log.service, 70);
+        serviceNameUIField.setFormTitleFunction(() => "Service");
+        const serviceNameFormField = new UIFormField(this.#serviceNameDataField, "Name");
+
+        serviceNameUIField.addDefaultAction("Update");
+        serviceNameUIField.addFormField(serviceNameFormField);
+
+        // Logging Level Field
+        const logLevelUIField = new UIForm((log) => log.level, 30);
+        logLevelUIField.setFormTitleFunction(() => "Select new level");
+
+        const setToInfoAction = new UIFormAction(LogLevel.INFO);
+        const setToWarnAction = new UIFormAction(LogLevel.WARN);
+        const setToErrorAction = new UIFormAction(LogLevel.ERROR);
+        const setToDebugAction = new UIFormAction(LogLevel.DEBUG);
+
+        setToInfoAction.addCallback(this.#logLevelDataField, (log) => log.level = LogLevel.INFO);
+        setToWarnAction.addCallback(this.#logLevelDataField, (log) => log.level = LogLevel.WARN);
+        setToErrorAction.addCallback(this.#logLevelDataField, (log) => log.level = LogLevel.ERROR);
+        setToDebugAction.addCallback(this.#logLevelDataField, (log) => log.level = LogLevel.DEBUG);
+
+        logLevelUIField.addFormAction(setToInfoAction);
+        logLevelUIField.addFormAction(setToWarnAction);
+        logLevelUIField.addFormAction(setToErrorAction);
+        logLevelUIField.addFormAction(setToDebugAction);
+
+        // Delete Field
+        const deleteUIField = new UIDeleteRowField(() => "X", 15);
+
+        return [
+            serviceNameUIField,
+            logLevelUIField,
+            deleteUIField
+        ];
+    }
+}
+
+
+await main();
+Script.complete();
